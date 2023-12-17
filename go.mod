99c5bf969b474dc6210d3775f6f2dd21228cd4d4
rajnautiyal <nautiyal.rajendra@gmail.com>
2023-12-17 11:06:50 +0000

    committing all the file

---
+++ go.mod
@@ -0,0 +1,7 @@
+module myexample
+
+go 1.21.0
+
+require github.com/GoesToEleven/puppy v1.3.0
+
+require github.com/GoesToEleven/dog v0.0.0-20230428023317-90bef1c76cb9 // indirect
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/GoesToEleven/puppy"
+)
+
+func main() {
+	fmt.Print("hello rajendra")
+	s1 := puppy.Bark()
+	s2 := puppy.BigBark()
+	fmt.Println(s1)
+	fmt.Println(s2)
+}
